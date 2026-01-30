65b19c4aea3730994db96b75e8e6c1ee02760d93
Danmur <hirohelper75@gmail.com>
2026-01-30 09:49:45 +0500

    add service layer and http handlers

--- internal/service/task_service.go
+++ internal/service/task_service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"Assignment3/internal/models"
 	"Assignment3/internal/repository"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type TaskService struct {
@@ -13,22 +14,22 @@ func NewTaskService(repo repository.TaskRepository) *TaskService {
 	return &TaskService{repo: repo}
 }
 
-func (s *TaskService) Create(task models.Task) models.Task {
+func (s *TaskService) Create(task models.Task) (models.Task, error) {
 	return s.repo.Create(task)
 }
 
-func (s *TaskService) GetAll() []models.Task {
+func (s *TaskService) GetAll() ([]models.Task, error) {
 	return s.repo.GetAll()
 }
 
-func (s *TaskService) GetByID(id int) (models.Task, bool) {
+func (s *TaskService) GetByID(id primitive.ObjectID) (models.Task, error) {
 	return s.repo.GetByID(id)
 }
 
-func (s *TaskService) Update(id int, task models.Task) (models.Task, bool) {
+func (s *TaskService) Update(id primitive.ObjectID, task models.Task) (models.Task, error) {
 	return s.repo.Update(id, task)
 }
 
-func (s *TaskService) Delete(id int) bool {
+func (s *TaskService) Delete(id primitive.ObjectID) error {
 	return s.repo.Delete(id)
 }
